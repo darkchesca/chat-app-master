0b03c8b3dbfa89a005d5db438f0973822bf64b4c
francesca.boe <francesca.boe@outlook.com>
2020-08-27 15:50:17 +0200

    add fire credentials

--- src/fire.js
+++ src/fire.js
@@ -2,12 +2,13 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 var config = {
-  apiKey: "your key",
-  authDomain: "your domian",
-  databaseURL: "hyour database url",
-  projectId: "your project id",
-  storageBucket: "your buget",
-  messagingSenderId: "your messaginId",
+  apiKey: "AIzaSyAihazq70vJmAms4e1Mtkog-tg60hsBAPo",
+  authDomain: "my-first-firebase-projec-696c1.firebaseapp.com",
+  databaseURL: "https://my-first-firebase-projec-696c1.firebaseio.com",
+  projectId: "my-first-firebase-projec-696c1",
+  storageBucket: "my-first-firebase-projec-696c1.appspot.com",
+  messagingSenderId: "92769039584",
+  appId: "1:92769039584:web:d3d0a6baeeb353ca340c85"
 };
 
 var fire = firebase.initializeApp(config);
